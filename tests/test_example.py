7529bcd4ed83957cb4ab5cf87bb2b851ff554d4c
Paullux <paulwoisard@gmail.com>
2024-05-19 13:15:57 +0200

    automatisation de la création des binaires

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,17 +1,36 @@
+import sys
 import os
 import unittest
-from src.MainWindow import MainWindow  # Assurez-vous de pointer vers l'emplacement correct de votre classe
+from unittest.mock import patch
 
-class MainWindow(unittest.TestCase):
+# Ajouter dynamiquement le chemin du module 'src'
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
 
-    def test_get_ordered_uuids(self):
+from MainWindow import MainWindow  # Assurez-vous de pointer vers l'emplacement correct de votre classe
+
+class TestMainWindow(unittest.TestCase):
+
+    @patch('requests.get')
+    def test_get_ordered_uuids(self, mock_get):
         if os.getenv('CI'):
             self.skipTest('Skipping network test in CI environment')
+
+        # Mock the API response
+        mock_get.return_value.status_code = 200
+        mock_get.return_value.json.return_value = {
+            "result": {
+                "uuid1": {"name": "France 2"},
+                "uuid2": {"name": "France 3"}
+            }
+        }
 
         app = MainWindow()
         ordered_uuids = app.get_ordered_uuids()
 
         # Ajoutez ici vos assertions pour vérifier les résultats
+        self.assertEqual(len(ordered_uuids), 2)
+        self.assertEqual(ordered_uuids[0][1], "France 2")
+        self.assertEqual(ordered_uuids[1][1], "France 3")
 
 if __name__ == '__main__':
     unittest.main()
