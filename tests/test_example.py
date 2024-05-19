3155deb31319ecb031649a65488d3e1d4e8e14ac
Paullux <paulwoisard@gmail.com>
2024-05-19 13:10:30 +0200

    automatisation de la création des binaires

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,3 +1,17 @@
-# tests/test_example.py
-def test_example():
-    assert 1 + 1 == 2
+import os
+import unittest
+from src.MainWindow import MainWindow  # Assurez-vous de pointer vers l'emplacement correct de votre classe
+
+class MainWindow(unittest.TestCase):
+
+    def test_get_ordered_uuids(self):
+        if os.getenv('CI'):
+            self.skipTest('Skipping network test in CI environment')
+
+        app = MainWindow()
+        ordered_uuids = app.get_ordered_uuids()
+
+        # Ajoutez ici vos assertions pour vérifier les résultats
+
+if __name__ == '__main__':
+    unittest.main()
